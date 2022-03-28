6f17c900e96d55276b9b0733e0c3eb43001f15b6
Muhammad Farhan Haykal <m.farhan.haykal@gmail.com>
2022-03-28 19:19:14 +0700

    Add-feature : Fix get and remove server-terminal
    Fix the get command for the server-side and remove output message on server-terminal

--- server-client.py
+++ server-client.py
@@ -7,6 +7,7 @@ import argparse, socket
 import sys
 import time
 import glob
+import os
 # import string
 
 def recvall(sock, length):
@@ -26,62 +27,80 @@ def server(interface, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((interface, port))
-        sock.listen(0)
+        sock.listen(1)
         sc, sockname = sock.accept()
-        print('Waiting to accept a new connection...')
-        # sc, sockname = sock.accept()
         if c==0:
+            print('Waiting to accept a new connection...')
             print('We have accepted a connection from', sockname)
             c+=1
         print('Socket name:', sc.getsockname())
         print('Socket peer:', sc.getpeername())
         len_msg = recvall(sc, 3)
         message = recvall(sc, int(len_msg))
+
+        #Decode and split the message from client
         acc_message = message.decode()
         str_message = acc_message.split()
 
+        #Condition for ping command
         if str_message[0] == 'ping':
-            # sc.sendall(message[1:])
             str_messageJoin = ' '.join(str_message[1:])
-            # print('Terima:')
-            print('Terima: ' + str_messageJoin)
-            bit_message = str_messageJoin.encode()
+            format1 = "terima: "
+            bit_message = format1.encode() + str_messageJoin.encode()
+            len_bit_message = b"%03d" % (len(bit_message.decode()),)
+            sc.sendall(len_bit_message)
             sc.sendall(bit_message)
 
-        if str_message[0] == "ls":
-            
+        #Condition to follow for ls command
+        elif str_message[0] == "ls":
+            #Condition to follow for ls-only
             if len(str_message) == 1:
                 files = '*'
+            #Condition to follow for ls with follow-up path
             elif len(str_message) > 1:
                 files = str_message[1]
 
             listed_files = glob.glob(files,recursive=True)
             return_files = ''
-            
             for i in listed_files:
-                return_files += i + '\n'
-            print(return_files)
+                basename = os.path.basename(i)
+                return_files += basename + '\n'
             len_ret_files = b"%03d" % (len(return_files))
             sc.sendall(len_ret_files)
             bit_return_files = return_files.encode()
             sc.sendall(bit_return_files)
 
-        if str_message[0] == "get":
-            listed_files = glob.glob(str_message[1])
-            size = str(len(listed_files))
-            return_message = "fetch:" + str_message[1] + " size:" + size + " lokal:" + str_message[2]
-            print(return_message)
-            bit_message = return_message.encode()
-            sc.sendall(bit_message)
+        #Condition to follow for get command
+        elif str_message[0] == "get":
+            #Assign the path input to variable p
+            p = os.path.dirname(str_message[1])
+            #Assign the filename input to variable names
+            names = str_message[2]
+            #Initiate variable for length counter
+            sizes = 0
+            #Search the file in the path directory p
+            for files in os.scandir(p):
+                basename = os.path.basename(files)
+                if basename.startswith(str_message[2]):
+                    #Open, read and count the length of the files
+                    f = open(files,"rb")
+                    b = f.read()
+                    sizes += len(b)
+                    f.close()
+            size = str(sizes)
+            space =" "
+            format1 = "fetch: "
+            format2 = "size: "
+            format3 = "lokal: "
+            #Encode the return messsage for client
+            return_message = format1.encode() + p.encode() + space.encode() + format2.encode() + size.encode() + space.encode() + format3.encode() + names.encode()
+            len_ret_message = b"%03d" % (len(return_message.decode()),)
+            sc.sendall(len_ret_message)
+            sc.sendall(return_message)
+        #Condition to follow for quit command
+        elif str_message[0] == "quit":
             print("Server shutdown..")
-            time.sleep(1)
-            print("Client shutdown..")
-            sys.exit(0)
-
-        if str_message[0] == "quit":
-            print("Server shutdown..")
-            time.sleep(1)
-            print("Client shutdown..")
+            sc.close()
             sys.exit(0)
 
 def client(host, port):
@@ -89,20 +108,25 @@ def client(host, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
         print('Client has been assigned socket name', sock.getsockname())
+        #Input and split message
         input_msg = input("> ")
         msgSplit = input_msg.split()
        
+        #Condition to follow for if command
         if msgSplit[0] == "ping":
             msgJoin = ' '.join(msgSplit)
             msg = msgJoin.encode()
             len_msg = b"%03d" % (len(msg),)
             msg = len_msg + msg
             sock.sendall(msg)
-            reply = recvall(sock, len(msg)-8)
-            reply1 = reply.decode()
+            len_recv = recvall(sock, 3)
+            msg_recv = recvall(sock, int(len_recv))
+            reply1 = msg_recv.decode()
             print(reply1)
 
-        if msgSplit[0] == "ls":
+        #Condition to follow for ls command
+        elif msgSplit[0] == "ls":
+            #Condition to follow for ls-only
             if len(msgSplit) == 1:
                 msg = input_msg.encode()
                 len_msg = b"%03d" % (len(msg),)
@@ -112,6 +136,8 @@ def client(host, port):
                 msg_recv = recvall(sock, int(len_recv))
                 reply1 = msg_recv.decode()
                 print(reply1)
+
+            #Condition to follow for ls with follow-up path
             elif len(msgSplit) > 1:
                 msgJoin = ' '.join(msgSplit)
                 msg = msgJoin.encode()
@@ -123,29 +149,27 @@ def client(host, port):
                 reply1 = msg_recv.decode()
                 print(reply1)
 
-        if msgSplit[0] == "get":
+        #Condition to follow for get command
+        elif msgSplit[0] == "get":
             msgJoin = ' '.join(msgSplit)
             msg = msgJoin.encode()
             len_msg = b"%03d" % (len(msg),)
             msg = len_msg + msg
             sock.sendall(msg)
-            len_reply = recvall(sock,3)
-            reply = recvall(sock, int(len_reply))
-            reply1 = reply.decode()
-            print(repr(reply1))
-            print("Server shutdown..")
-            time.sleep(2)
-            print("Client shutdown..")
-            sys.exit(0)
+            len_recv = recvall(sock, 3)
+            msg_recv = recvall(sock, int(len_recv))
+            reply1 = msg_recv.decode()
+            print(reply1)
 
-        if msgSplit[0] == "quit":
+        #Condition to follow for quit command
+        elif msgSplit[0] == "quit":
             msg = input_msg.encode()
             len_msg = b"%03d" % (len(msg),)
             msg = len_msg + msg
-            sock.sendall(msg)
-            print('Server shutdown...') 
+            sock.sendall(msg) 
             time.sleep(2)
             print('Client shutdown...')
+            sock.close()
             sys.exit(0)
 
 if __name__ == '__main__':
